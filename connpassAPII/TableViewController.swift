b3f6aa8a3bbf1327039c39dbc4f56e529e1dc62d
Erina Taguchi <kd1257044@st.kobedenshi.ac.jp>
2018-11-16 12:06:47 +0900

    foundation

---
+++ connpassAPII/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  connpassAPII
+//
+//  Created by 田口絵理奈 on 2018/11/16.
+//  Copyright © 2018年 田口絵理奈. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- connpassAPII/ViewController.swift
+++ connpassAPII/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var contact: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func SendAPI(_ sender: Any) {
+    }
+    
 }
 
