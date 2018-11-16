e182f0ffb514cef1e367b03561aaf8076d125954
Erina Taguchi <kd1257044@st.kobedenshi.ac.jp>
2018-11-16 11:34:08 +0900

    Initial Commit

---
+++ connpassAPII/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  connpassAPII
+//
+//  Created by 田口絵理奈 on 2018/11/16.
+//  Copyright © 2018年 田口絵理奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ connpassAPIITests/connpassAPIITests.swift
@@ -0,0 +1,34 @@
+//
+//  connpassAPIITests.swift
+//  connpassAPIITests
+//
+//  Created by 田口絵理奈 on 2018/11/16.
+//  Copyright © 2018年 田口絵理奈. All rights reserved.
+//
+
+import XCTest
+@testable import connpassAPII
+
+class connpassAPIITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
